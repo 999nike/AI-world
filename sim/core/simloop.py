7d47695985a61e2d3aa50119551546d6d9cce18e
999nike <999nike@gmail.com>
2026-08-17 15:40:04 +0100

    E5.1 Observatory: core implementation

--- sim/core/simloop.py
+++ sim/core/simloop.py
@@ -14,7 +14,7 @@ from sim.core.build_governors import (
     resolve_building, can_build_hut, can_build_granary, can_build_mine, can_build_road,
     can_build_workshop, can_build_barracks, can_build_market, can_build_temple,
     can_build_academy, can_build_walls, can_build_irrigation, can_build_library,
-    can_build_foundry, can_build_hall, can_build_command, can_build_lab,
+    can_build_foundry, can_build_hall, can_build_command, can_build_lab, can_build_observatory,
 )
 from sim.core.governor import Governor
 from sim.core.scenario import Scenario
@@ -41,12 +41,13 @@ BUILD_COSTS = {
     "foundry": {"wood": 3, "stone": 3},
     "hall": {"wood": 3, "stone": 3},
     "command": {"wood": 3, "stone": 4},
-    "lab": {"wood": 4, "stone": 4},  # E5.0
+    "lab": {"wood": 4, "stone": 4},
+    "observatory": {"wood": 5, "stone": 4},  # E5.1
 }
 
 STACKABLE = {"storage", "farm", "granary", "mine", "road", "workshop", "barracks",
              "market", "temple", "academy", "walls", "irrigation", "library",
-             "foundry", "hall", "command", "lab"}
+             "foundry", "hall", "command", "lab", "observatory"}
 
 
 def run_sim(
@@ -108,7 +109,7 @@ def run_sim(
         "build_granary": 0, "build_mine": 0, "build_road": 0, "build_workshop": 0, "build_barracks": 0,
         "build_market": 0, "build_temple": 0, "build_academy": 0, "build_walls": 0,
         "build_irrigation": 0, "build_library": 0, "build_foundry": 0, "build_hall": 0, "build_command": 0,
-        "build_lab": 0,
+        "build_lab": 0, "build_observatory": 0,
         "farm_harvest_events": 0, "farm_food_total": 0,
         "granary_food_total": 0, "mine_stone_total": 0, "workshop_tools_total": 0, "barracks_soldiers_total": 0,
         "tools_boost_events": 0, "soldier_defend_events": 0, "raid_events": 0, "raid_loot_total": 0,
@@ -259,6 +260,9 @@ def run_sim(
                     if not allowed: ok, note = False, gate_note
                 elif b == "lab":
                     allowed, gate_note = can_build_lab(a.x, a.y, sm, world)
+                    if not allowed: ok, note = False, gate_note
+                elif b == "observatory":
+                    allowed, gate_note = can_build_observatory(a.x, a.y, sm, world)
                     if not allowed: ok, note = False, gate_note
 
                 if ok and b not in BUILD_COSTS:
